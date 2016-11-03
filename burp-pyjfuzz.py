1ed2a6b99c853016874bd27af95715f5dd289299
Daniele Linguaglossa <danielelinguaglossa@gmail.com>
2016-11-03 13:25:58 +0100

    Updated due to PyJFuzz update

--- burp-pyjfuzz.py
+++ burp-pyjfuzz.py
@@ -193,7 +193,7 @@ class JSONFuzzer(IIntruderPayloadGenerator):
     def fuzz(self, original_payload):
         # Call PyJFuzz
         original_payload = urllib.unquote(original_payload)
-        p1 = subprocess.Popen([self.pyjfuzz, '-j', original_payload] + self._args, stdout=subprocess.PIPE)
+        p1 = subprocess.Popen([self.pyjfuzz, '--j', original_payload] + self._args, stdout=subprocess.PIPE)
         output = p1.communicate()
         p1.stdout.close()
         del p1
